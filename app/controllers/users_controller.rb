c06f2f4ed81f7cb9dd668bd02ba4495a9f0beb10
kshiyono <k.shiyo.no@gmail.com>
2019-10-13 16:30:43 +0900

    [fix] sample_app - ログインユーザのみ更新処理できるように修正・テスト

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,29 +1,31 @@
 class UsersController < ApplicationController
 
-  def show
-    @user = User.find(params[:id])
-  end
+  before_action :logged_in_user, only: [:edit, :update]
 
-  def new
-    @user = User.new
-  end
-
-  def create
-    @user = User.new(user_params)
-    if @user.save
-      log_in @user
-      flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user
-    else
-      render 'new'
+    def show
+      @user = User.find(params[:id])
     end
-  end
 
-  def edit
-    @user = User.find(params[:id])
-  end
+    def new
+      @user = User.new
+    end
 
-  def update
+    def create
+      @user = User.new(user_params)
+      if @user.save
+        log_in @user
+        flash[:success] = "Welcome to the Sample App!"
+        redirect_to @user
+      else
+        render 'new'
+      end
+    end
+
+    def edit
+      @user = User.find(params[:id])
+    end
+
+    def update
       @user = User.find(params[:id])
       if @user.update_attributes(user_params)
         flash[:success] = "Profile updated"
@@ -38,5 +40,15 @@ class UsersController < ApplicationController
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
+    end
+
+    # beforeアクション
+
+    # ログイン済みユーザーかどうか確認
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
     end
 end
