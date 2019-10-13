01831acccec14b50b2cdf7a0b8a9713d404267b2
kshiyono <k.shiyo.no@gmail.com>
2019-10-13 17:40:25 +0900

    [fix] sample_app - ユーザの一覧表示処理の適応

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,7 @@ class UsersController < ApplicationController
   before_action :correct_user,   only: [:edit, :update]
 
   def index
+    @users = User.paginate(page: params[:page])
   end
 
   def show
@@ -61,9 +62,5 @@ class UsersController < ApplicationController
   def correct_user
     @user = User.find(params[:id])
     redirect_to(root_url) unless current_user?(@user)
-  end
-
-  def index
-    @users = User.all
   end
 end
