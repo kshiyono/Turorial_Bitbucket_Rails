2c59992f7990b107d583df7c6b4d532038b53de1
kshiyono <k.shiyo.no@gmail.com>
2019-10-19 16:05:57 +0900

    [fix] sample_app - ユーザ見ログイン時のマイクロポスト作成テスト

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -54,30 +54,21 @@ class UsersController < ApplicationController
 
   private
 
-  def user_params
-    params.require(:user).permit(:name, :email, :password,
-                                 :password_confirmation)
-  end
-
-  # beforeアクション
-
-  # ログイン済みユーザーかどうか確認
-  def logged_in_user
-    unless logged_in?
-      store_location
-      flash[:danger] = "Please log in."
-      redirect_to login_url
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
     end
-  end
 
-  # 正しいユーザーかどうか確認
-  def correct_user
-    @user = User.find(params[:id])
-    redirect_to(root_url) unless current_user?(@user)
-  end
+    # beforeフィルター
 
-  # 管理者かどうか確認
-  def admin_user
-    redirect_to(root_url) unless current_user.admin?
-  end
+    # 正しいユーザーかどうかを確認
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
+
+    # 管理者かどうかを確認
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
+    end
 end
